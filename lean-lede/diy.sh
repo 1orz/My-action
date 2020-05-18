27215429ca25463863720f25e830e931606c1426
1orz <cloudorzi@gmail.com>
2020-05-18 18:30:14 +0800

    fix

--- lean-lede/diy.sh
+++ lean-lede/diy.sh
@@ -2,6 +2,7 @@
 # 本脚本工作目录必须是git仓库的主目录
 
 # Add Some Package
+
 mkdir -p package/custom
 cd package/custom
 # git clone https://github.com/coolsnowwolf/packages
@@ -18,9 +19,11 @@ git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
 cd ../../
 
 # Modify default IP
+
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 # Add some default settings
+
 #\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
 curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
