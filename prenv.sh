9bb86f04d4b94fc48c82c7156709cb7a221efbeb
1orz <cloudorzi@gmail.com>
2025-10-04 10:50:56 +0800

    ci: update

--- prenv.sh
+++ prenv.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
+sudo rm -rf /usr/share/dotnet /usr/local/lib/android /opt/ghc
 sudo apt update
-sudo apt install libncurses-dev gawk gettext clang llvm flex g++ gawk \
-    gettext git libssl-dev python3-pyelftools python3-setuptools \
-    python3-dev python3-pip rsync unzip zlib1g-dev swig aria2 jq subversion qemu-utils ccache rename \
-    libelf-dev device-tree-compiler libgnutls28-dev coccinelle libgmp3-dev libmpc-dev libfuse-dev
+# 安装基础开发包
+sudo apt install libncurses-dev gawk gettext-base clang llvm flex g++ \
+    git libssl-dev python3-setuptools \
+    python3-dev python3-pip rsync unzip zlib1g-dev swig aria2 jq subversion qemu-utils rename \
+    libelf-dev libgnutls28-dev coccinelle libgmp-dev libmpc-dev libfuse-dev
+
+# 安装需要通过snap安装的包
+# sudo snap install ccache device-tree-compiler
+
+# 安装python3-pyelftools (通过pip)
+# pip3 install pyelftools
 sudo apt autoremove
 sudo apt clean
