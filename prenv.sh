7359bd5dd9259a64d76b6ee111a121a28f3de9da
Pengxin.Liu <cloudorzi@gmail.com>
2025-10-06 22:49:43 +0800

    ci: update

--- prenv.sh
+++ prenv.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install libncurses-dev gawk gettext-base clang llvm flex g++ \
     git libssl-dev python3-setuptools \
     python3-dev python3-pip rsync unzip zlib1g-dev swig aria2 jq subversion qemu-utils rename \
-    libelf-dev libgnutls28-dev coccinelle libgmp-dev libmpc-dev libfuse-dev
+    libelf-dev libgnutls28-dev coccinelle libgmp-dev libmpc-dev libfuse-dev neofetch
 
 # 安装需要通过snap安装的包
 # sudo snap install ccache device-tree-compiler
