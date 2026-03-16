f5e24eeb67d09c8481490c4f36c564924aa356e1
1orz <cloudorzi@gmail.com>
2026-03-16 20:39:04 +0800

    fix

--- prenv.sh
+++ prenv.sh
@@ -2,8 +2,8 @@
 sudo rm -rf /usr/share/dotnet /usr/local/lib/android /opt/ghc
 sudo apt update
 
-sudo apt install libncurses-dev gawk gettext-base clang llvm flex g++ \
-    git libssl-dev python3-setuptools \
+sudo apt install -y libncurses-dev gawk gettext-base clang llvm flex g++ \
+    git libssl-dev python3-setuptools ccache \
     python3-dev python3-pip rsync unzip zlib1g-dev swig aria2 jq subversion qemu-utils rename \
     libelf-dev libgnutls28-dev coccinelle libgmp-dev libmpc-dev libfuse-dev neofetch
 
